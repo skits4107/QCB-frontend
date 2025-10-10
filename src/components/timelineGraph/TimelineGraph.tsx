95173316c94a5ba1c7cefb0f8f1fcbc692e94234
skits4107 <aaronjosua719@gmail.com>
2025-10-09 20:47:50 -0700

    using state for tree.

--- src/components/timelineGraph/TimelineGraph.tsx
+++ src/components/timelineGraph/TimelineGraph.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import TimelineNode from "../node/TimelineNode";
 
@@ -5,14 +6,14 @@ import TimelineNode from "../node/TimelineNode";
 function TimelineGraph(){
 
     //example tree data for now. get from backend later (would be reutning as json fomrated like this).
-    let tree:Record<number, Record<string, number[]|string|boolean>> = {
+    let [tree, setTree] = useState({
         0: {"children":[1,2], "data":"should I eat a burger or a milk shake", "live":true},
         1: {"children":[3], "data":"burger", "live":true},
         2: {"children":[], "data":"milk shake", "live":false},
         3: {"children":[4,5], "data":"should I watch show Y or Show X", "live":true},
         4: {"children":[], "data":"Y", "live":false},
         5: {"children":[], "data":"X", "live":true}
-    }
+    })
 
     //TOOD: generate array of node and link positions.
 

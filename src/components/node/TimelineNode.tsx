2f0b5cb3a9f662819a3845c425d673f1f1154f4d
skits4107 <aaronjosua719@gmail.com>
2025-10-09 20:15:40 -0700

    changed node component. added testing data.

--- src/components/node/TimelineNode.tsx
+++ src/components/node/TimelineNode.tsx
@@ -2,8 +2,7 @@ import type { JSX } from "react";
 import './TimelineNode.css'
 
 type NodeData = {
-    answer?:string,
-    question?:string,
+    data:string,
     x:number,
     y:number
 }
@@ -13,36 +12,22 @@ function truncateText(text: string, maxChars: number) {
   return text.slice(0, maxChars - 3) + "...";
 }
 
-function TimelineNode({answer, question, x, y}:NodeData){
-    let content:JSX.Element[] = [];
+function TimelineNode({data, x, y}:NodeData){
+    let content;
     let radius:number = 50;
-    if (answer){
-        content.push(
-        <text 
-        key="answer" 
-        className="NodeText"
-        x={x}
-        y={y}
-        textAnchor="middle"
-        dominantBaseline="middle"
-        fill="white">
-            {truncateText(answer, 10)}
-        </text>
-        );
+    if (data){
+        content =  <text 
+                    key="answer" 
+                    className="NodeText"
+                    x={x}
+                    y={y}
+                    textAnchor="middle"
+                    dominantBaseline="middle"
+                    fill="white">
+                        {truncateText(data, 10)}
+                    </text>;
     }
-    if (question){
-        content.push(<text 
-            key="question"
-            className="NodeText"
-            x={x}
-            y={y+20}
-            textAnchor="middle"
-            dominantBaseline="middle"
-            fill="white">
-                {truncateText(question, 10)}
-            </text>
-            );
-    }
+    
     return (
         <>
             <svg width="100vw" height="100vh">
--- src/components/timelineGraph/TimelineGraph.tsx
+++ src/components/timelineGraph/TimelineGraph.tsx
@@ -2,14 +2,30 @@ import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import '../node/TimelineNode'
 import TimelineNode from "../node/TimelineNode";
 
+
 function TimelineGraph(){
-    //buttons for testing. replace with actual timeline graph later.
+
+    //example tree data for now. get from backend later.
+    let tree:Record<number, Record<string, number[]|string>> = {
+        0: {"children":[1,2], "data":"should I eat a burger or a milk shake"},
+        1: {"children":[3], "data":"burger"},
+        2: {"children":[], "data":"milk shake"},
+        3: {"children":[4,5], "data":"should I watch show Y or Show X"},
+        4: {"children":[], "data":"Y"},
+        5: {"children":[], "data":"X"}
+    }
+
     return (
         <TransformWrapper limitToBounds={false}>
             <TransformComponent>
+                {
+                    
+                }
+
+
                 {/* exmaple of node. nodes will be generated from data in future*/}
                 <div style={{ width: "100vw", height: "100vh", position: "relative"}}>
-                  <TimelineNode answer="cheesefi3n i" question="root question. do you like cheese or apples?" x={500} y={500}/>
+                  <TimelineNode data="root question. do you like cheese or apples?" x={700} y={100}/>
                 </div>
             </TransformComponent>
         </TransformWrapper>
